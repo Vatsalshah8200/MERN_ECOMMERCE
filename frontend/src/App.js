2de7893df9e752ce0fc947e8942c507236198de9
Vatsalshah8200 <202012042@daiict.ac.in>
2021-05-09 02:27:24 +0530

    trying frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
 
 import './App.css';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import Navbar from "./components/Navbar"
 function App() {
   return (
-    <div className="App">
-     Hello World
-    </div>
+    <Router>
+     {/*Navbar*/ }
+     <Navbar></Navbar>
+      {/*SideDrawer*/ }
+      {/*backdrop*/}
+      <main>
+        <Switch>
+          <Route exact path="/" componenet={HomeScreen}/>
+          <Route exact path="/product/:id" component={ProductScreen}/>
+          <Route exact path="/cart" component={CartScreen}/>
+
+        </Switch>
+      </main>
+      </Router>
+    
   );
 }
 
